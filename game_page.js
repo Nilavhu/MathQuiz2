034ecac8f364a061f71c6fb55348b8f05a4d97ae
nilnilnilnil12321 <74089182+nilnilnilnil12321@users.noreply.github.com>
2022-01-10 14:26:38 +0400

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -1,6 +1,6 @@
 
-	player1_name = localStorage.getItem("player1_name");
-	player2_name = localStorage.getItem("player2_name");
+	player1_name = localStorage.getItem("name_1");
+	player2_name = localStorage.getItem("name_2");
 
 	player1_score = 0;
 	player2_score = 0;
